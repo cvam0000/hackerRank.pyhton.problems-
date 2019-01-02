013508bca489464580a1c0ec0ee458deef50d1d2
cvam0000 <cvam0000@gmail.com>
2019-01-03 01:55:48 +0530

    update

---
+++ cuboid.py
@@ -0,0 +1,16 @@
+x=int(input())
+y=int(input())
+z=int(input())
+n=int(input())
+xr=[]
+yr=[]
+zr=[]
+for lehsun in range (0,x):
+	xr.append(lehsun)
+for lehsun in range (0,y):
+	yr.append(lehsun)
+for lehsun in range (0,y):
+	zr.append(lehsun)
+print(xr)
+print(yr)
+print(zr)
