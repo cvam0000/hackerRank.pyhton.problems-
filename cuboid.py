43f15fa9d2b89e19cba70d921879daec4f1ce2b1
cvam0000 <cvam0000@gmail.com>
2019-01-03 03:24:16 +0530

    update

--- cuboid.py
+++ cuboid.py
@@ -2,15 +2,23 @@ x=int(input())
 y=int(input())
 z=int(input())
 n=int(input())
+if x>y and z>y:
+	chotu=y
+if y>x and z>x:
+	chotu=x
+else:
+	chotu=z 
 xr=[]
 yr=[]
 zr=[]
+result=[]
+sub_result=[]
 for lehsun in range (0,x):
 	xr.append(lehsun)
 for lehsun in range (0,y):
 	yr.append(lehsun)
 for lehsun in range (0,y):
 	zr.append(lehsun)
-print(xr)
-print(yr)
-print(zr)
+for lehsun in range (0,chotu):
+	 if xr[lehsun]+yr[lehsun]+zr[lehsun]!=n:
+		sub_result.apped()
