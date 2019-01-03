dba8ef6da0b0ebb0cdcf1e6643f6e9d8d769e973
cvam0000 <cvam0000@gmail.com>
2019-01-03 21:35:44 +0530

    update

--- cuboid.py
+++ cuboid.py
@@ -2,27 +2,39 @@ x=int(input())
 y=int(input())
 z=int(input())
 n=int(input())
-if x>y and z>y:
-	chotu=y
-if y>x and z>x:
-	chotu=x
-else:
-	chotu=z
-xr=[]
-yr=[]
-zr=[]
 result=[]
-sub_result=[]
-for lehsun in range (0,x):
-	xr.append(lehsun)
-for lehsun in range (0,y):
-	yr.append(lehsun)
-for lehsun in range (0,y):
-	zr.append(lehsun)
-for lehsun in range (0,chotu):
-	a=xr[lehsun]
-	b=yr[lehsun]
-	c=zr[lehsun]
-	if a+b+c != n:
-		result.append(a,b,c)
-print(result)		
+big=max(x,y,z)
+
+
+def remove_duplicates(values):
+    output = []
+    seen = set()
+    for value in values:
+
+        if value not in seen:
+            output.append(value)
+            seen.add(value)
+    print(output)        
+
+
+def funky(a,b,c):
+	for u in range (0,big):
+
+		for lehsun in range (0,z+1):
+			result.append((a,b,c))
+			c+=1
+		c=0
+		for lehsun in range (0,y+1):
+			result.append((a,b,c))
+			b+=1
+		b=0
+		for lehsun in range(0,x+1):
+			result.append((a,b,c))
+			a+=1
+		a=0
+
+for o in range (0,big):
+	funky(o,o,o)
+
+result.sort()
+g=remove_duplicates(result)
