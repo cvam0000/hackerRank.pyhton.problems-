c243dfe676055107e7ce8c3e2c70a08027afa5e8
cvam0000 <cvam0000@gmail.com>
2019-01-12 02:59:50 +0530

    update

--- minion.py
+++ minion.py
@@ -2,14 +2,17 @@ def minion_game(string):
     e=[]
     for i in string:
         e.append(i)
-    stuart=[]
-    kevin=[]
+    stuart=0
+    kevin=0
     different=[]
     vov=['A' , 'E' , 'I' , 'O'  ,'U']
     for i in string:
         if i not in different:
             different.append(i)
     print (different)
+
+    for i in different:
+        if i in vov:
 
 
 
